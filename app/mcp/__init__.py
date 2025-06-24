bf5cfe5a9ebc702c87160194ed737b98464f5f5e
Om Gupta <omgupta0720@gmail.com>
2025-06-24 14:55:36 +0530

    feat: running mcp server

--- app/mcp/__init__.py
+++ app/mcp/__init__.py
@@ -1,4 +1,4 @@
 from .server import huddle01_mcp
-from .client import client as http_client
+from .client import huddle_client
 
-__all__ = ["huddle01_mcp", "http_client"]
+__all__ = ["huddle01_mcp", "huddle_client"]
